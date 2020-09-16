d31b802140980e0b8f6e10a02a8ac6f9e69feabf
Kefan Yang <yang.3949@buckeyemail.osu.edu>
2020-09-16 18:59:33 -0400

    merge

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
-})
+});
 
 app.listen(8000, function(){
     console.log("app listen on port 8000");
-})
+});
