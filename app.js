372a6b5ad816f62d4b50312223503e716ba51bbc
Kefan Yang <yang.3949@buckeyemail.osu.edu>
2020-09-16 19:00:31 -0400

    nothing

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
-    res.send("Hello World!");
-});
-=======
     res.render('test');
 })
->>>>>>> e8c2e17041ba982aa488bc834db70167e69b8a9c
 
 app.listen(8000, function(){
     console.log("app listen on port 8000");
