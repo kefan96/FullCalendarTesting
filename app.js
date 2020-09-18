58748cc3e6303cafb64705fa308cb5cf672b03e0
Kefan Yang <yang.3949@buckeyemail.osu.edu>
2020-09-18 15:47:24 -0400

    calendar testing

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-
 app.get("/", (req, res) => {
     res.render('test');
 })
+
+app.get("/fullcalendar/main.css", (req, res) => {
+    res.sendFile(__dirname + '/node_modules/fullcalendar/main.css');
+});
+
+app.get("/fullcalendar/main.js", (req, res) => {
+    res.sendFile(__dirname + '/node_modules/fullcalendar/main.js');
+});
 
 app.listen(8000, function(){
     console.log("app listen on port 8000");
