cf94f65ddab5769f9d9bd2242a7472a1de343786
yifengjin89 <55426354+yifengjin89@users.noreply.github.com>
2020-09-19 01:52:45 -0400

    added modal for events

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.get("/fullcalendar/main.js", (req, res) => {
     res.sendFile(__dirname + '/node_modules/fullcalendar/main.js');
 });
 
+app.get("/jq-paginator/dist/jq-paginator.min.js", (req, res) => {
+    res.sendFile(__dirname + '/node_modules/jq-paginator/dist/jq-paginator.min.js');
+});
+
+app.get("/jquery/dist/jquery.min.js", (req, res) => {
+    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
+});
+
+
+
+
 app.listen(8000, function(){
     console.log("app listen on port 8000");
 });
